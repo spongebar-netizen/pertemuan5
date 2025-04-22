5133716ad8c7f9c4d9d1ef6f4d9b1db20ce34cf3
spongebar-netizen <fatanjrz1@gmail.com>
2025-04-22 16:17:14 +0700

    first commit

---
+++ node.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video shingeki no kyojin");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio shingeki no kyojin");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar shingeki no kyojin");
+});
